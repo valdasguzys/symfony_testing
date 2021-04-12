0f7e7af5945bf88ddcd3dee1925e12ffec5f6d54
Valdas Gužys <valdas.guzys@gmail.com>
2021-04-12 13:53:35 +0300

    setup symfony project with testing environment

---
+++ tests/BookManagementTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class BookManagementTest extends KernelTestCase 
+{
+    public function SetUp(): void
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testItWorks()
+    {
+        $this->assertTrue(true);
+    }
+}
